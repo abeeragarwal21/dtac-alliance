0dbebc35260552fb16ec825b84383d59e8ae236b
abeeragarwal21 <abeer.agarwal@outlook.com>
2025-08-03 20:59:06 +0100

    Stopped the body from scrolling when mobile navigation open

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,10 @@
+import { lock, unlock } from "tua-body-scroll-lock";
+
 // Accordions
 const accordions = document.querySelectorAll(".accordion");
 console.log(accordions);
 
-function setPanelIcon(panelIcon, isHidden) {
-    panelIcon.classList = `ph ph-${isHidden ? "plus" : "minus"}`;
-}
-
-function setChallengeStyles(accordionPanel, panelHeading, isHidden) {
+function setChallengesAccordionStyles(accordionPanel, panelHeading, isHidden) {
     if (isHidden) {
         accordionPanel.style.backgroundColor = "transparent";
         panelHeading.style.color = "#fff";
@@ -26,12 +24,17 @@ accordions.forEach((accordion) => {
 
         const panelIcon = accordionPanel.querySelector(".ph");
         const panelHeading = accordionPanel.querySelector(".panel__heading");
-        const isHidden = panelContent.classList.contains("hidden");
 
-        setPanelIcon(panelIcon, isHidden);
+        panelIcon.classList = `ph ph-${
+            panelContent.classList.contains("hidden") ? "plus" : "minus"
+        }`;
 
         if (accordion.classList.contains("accordion--challenges")) {
-            setChallengeStyles(accordionPanel, panelHeading, isHidden);
+            setChallengesAccordionStyles(
+                accordionPanel,
+                panelHeading,
+                panelContent.classList.contains("hidden")
+            );
         }
     });
 });
@@ -41,14 +44,18 @@ const mobileNavBtn = document.querySelector(".mobile-nav-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileNavBtnIcon = mobileNavBtn.querySelector(".ph");
 
-let scrollPosition = 0;
-
 function showHideMobileNav() {
     mobileMenu.classList.toggle("hidden");
     mobileNavBtnIcon.classList = "";
     mobileNavBtnIcon.classList = `ph ph-${
         mobileMenu.classList.contains("hidden") ? "list" : "x"
     }`;
+
+    if (mobileMenu.classList.contains("hidden")) {
+        unlock();
+    } else {
+        lock();
+    }
 }
 
 mobileNavBtn.addEventListener("click", function () {
